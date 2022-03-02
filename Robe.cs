d44d174685220d417ec98a499e4ba7f58f9c0b22
jaypetty <jayshaun2343@gmail.com>
2022-03-02 11:00:08 -0600

    finished through phase4

---
+++ Robe.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Quest
+{
+    public class Robe
+    {
+        public List<string> Color = new List<string>();
+        public int Length {get;set;}
+    }
+}
